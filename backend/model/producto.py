c1bec1bd6db40f5d22cadb540ad83649d5f11023
KenyiGM <sunted351@gmail.com>
2025-10-31 20:43:21 -0500

    fix(model_producto): corrige los tipos de fechas y agrega mas parametros a los datos

--- backend/model/producto.py
+++ backend/model/producto.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import Optional
 
 from sqlmodel import Field, SQLModel
@@ -5,10 +6,10 @@ from sqlmodel import Field, SQLModel
 
 class Producto(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    nombre: str = Field(index=True)
-    descripcion: str
-    precio: float
-    cantidad: int
-    activo: bool = True
-    fecha_creacion: Optional[str] = None
-    fecha_actualizacion: Optional[str] = None
+    nombre: str = Field(index=True, nullable=False, min_length=2)
+    descripcion: str = Field(nullable=False, min_length=4)
+    precio: float = Field(nullable=False, ge=0)
+    cantidad: int = Field(nullable=False, ge=0)
+    activo: bool = Field(default=True)
+    fecha_creacion: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    fecha_actualizacion: Optional[datetime] = Field(default_factory=lambda: datetime.now(timezone.utc))
