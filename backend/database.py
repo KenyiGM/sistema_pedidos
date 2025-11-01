ba5c3aaab2a5737b2ad97203bcc004af692d25e1
KenyiGM <sunted351@gmail.com>
2025-11-01 14:06:58 -0500

    Mejora y creacion de la base de datos y Corrigiendo el archivo config

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,11 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, Session
 from core.config import settings
 
 sqlite_url = settings.DATABASE_URL
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def get_session():
+    with Session(engine) as session:
+        yield session   
